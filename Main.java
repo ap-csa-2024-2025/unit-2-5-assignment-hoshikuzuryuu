cc947d037cc4262363f674f75038d0d1f74ca50f
hoshikuzuryuu <yaminoiroha@gmail.com>
2024-10-07 10:17:34 -0700

    whaaaaaaaaaat

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    String output = new String();
+    Scanner sc = new Scanner(System.in);
+    Circle c1 = new Circle(10.1);
+    Circle c2 = new Circle(14.0);
+    Circle c3 = new Circle(20.5);
+    output = c1.toString() + "\n" + c2.toString() + "\n" + c3.toString();
+    System.out.println(output);
   }
 }
